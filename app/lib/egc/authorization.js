eeafde804012477133aef600b2a990213a1898a4
monocle <hchw29@gmail.com>
2014-04-19 13:02:11 -0700

    create AuthorizationMixin

---
+++ app/lib/egc/authorization.js
@@ -0,0 +1,43 @@
+// TODO - _gapi put in for testing needs to
+// handle Em.run.next(). Get rid of it.
+export default Em.Mixin.create({
+  apiKey: null,
+  clientId: null,
+  scope: null,
+
+  gapiReady: false,
+  authorized: null,
+  accessToken: null,
+  _gapi: null,
+
+  setApiKey: function() {
+    if (!this.get('gapiReady')) { return; }
+
+    var apiKey = this.get('apiKey');
+    if (!apiKey) { return; }
+
+    gapi.client.setApiKey(apiKey);
+    this.set('_gapi', gapi);
+    Em.run.next(this, function() { this.authorize(true); });
+  }.observes('gapiReady').on('init'),
+
+  authorize: function(now) {
+    this.get('_gapi').auth.authorize({
+      client_id: this.get('clientId'),
+      scope: this.get('scope'),
+      immediate: now
+    }, this.handlAuthResult.bind(this));
+  },
+
+  handlAuthResult: function(res) {
+    if (res && !res.error) { return this.set('authorized', true); }
+    this.set('authorized', false);
+  },
+
+  setAccessToken: function() {
+    if (!this.get('authorized')) { return; }
+
+    this.set('accessToken', gapi.auth.getToken().access_token);
+  }.observes('authorized'),
+
+});
---
+++ tests/lib/egc/authorization-test.js
@@ -0,0 +1,90 @@
+import AuthorizationMixin from 'egc/lib/egc/authorization';
+
+var EGC = Em.Object.extend(AuthorizationMixin),
+    egc, gapi;
+
+function createEgc(params) {
+  egc = EGC.create({
+    apiKey: '1',
+    clientId: '2',
+    scope: 'foo',
+    gapiReady: true
+  });
+}
+
+module("AuthorizationMixin", {
+  setup: function() {
+    gapi = {
+      client: { setApiKey: Em.K },
+      auth: {
+        authorize: Em.K,
+        getToken: Em.K
+      }
+    };
+    window.gapi = gapi;
+  },
+  teardown: function() {
+    delete window.gapi;
+  }
+});
+
+test("apiKey gets set on the gapi client", function() {
+  gapi.client.setApiKey = function(key) { equal(key, '1'); };
+  createEgc();
+});
+
+asyncTest("an immediate gapi authorization attempt is made", function() {
+  stop();
+  var params = { client_id: '2', scope: 'foo', immediate: true };
+  gapi.auth.authorize = function(p) { deepEqual(p, params); start(); };
+  createEgc();
+  start();
+});
+
+test("authorized starts off as null", function() {
+  createEgc();
+  equal(egc.get('authorized'), null);
+});
+
+asyncTest("authorized is false if immediate authorization attempt fails", function() {
+  stop();
+  gapi.auth.authorize = function(params, callback) {
+    callback({ error: 'nope' });
+    start();
+
+    equal(egc.get('authorized'), false);
+  };
+
+  createEgc();
+  start();
+});
+
+asyncTest("authorized is true if immediate authorization attempt succeeds", function() {
+  stop();
+  gapi.auth.authorize = function(params, callback) {
+    callback({});
+    equal(egc.get('authorized'), true);
+    start();
+  };
+
+  createEgc();
+  start();
+});
+
+test("accessToken is set if authorized", function() {
+  gapi.auth.getToken = function() {
+    return { access_token: 'token' };
+  };
+
+  createEgc();
+  egc.set('authorized', true);
+  equal(egc.get('accessToken'), 'token');
+});
+
+test("accessToken is not set if unauthorized", function() {
+  gapi.auth.getToken = function() {};
+
+  createEgc();
+  egc.set('authorized', false);
+  ok(!egc.get('accessToken'))
+});
