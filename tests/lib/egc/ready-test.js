69a7032b30e72a282a3dab9dfc66a7ddd6718dc2
monocle <hchw29@gmail.com>
2014-04-19 09:17:20 -0700

    create ReadyMixin

---
+++ tests/lib/egc/ready-test.js
@@ -0,0 +1,39 @@
+import ReadyMixin from 'egc/lib/egc/ready';
+
+var EGC = Em.Object.extend(ReadyMixin),
+    egc;
+
+module("ReadyMixin", {
+  setup: function() {
+    egc = EGC.create();
+  },
+  teardown: function() {
+    delete window.gapi;
+    delete window.gapiReady;
+  }
+});
+
+test("is ready on create when window.gapi exists", function() {
+  window.gapi = {};
+  var egc = EGC.create();
+  ok(egc.get('gapiReady'));
+});
+
+test("is not ready on create when window.gapi does not exist", function() {
+  ok(!egc.get('gapiReady'));
+});
+
+test("sets window.gapiReady when window.gapi does not exist", function() {
+  ok(window.gapiReady);
+});
+
+test("becomes ready when window.gapiReady is called", function() {
+  window.gapiReady();
+  ok(egc.get('gapiReady'));
+});
+
+test("will call getScript with gapi source URL if window.gapi does not exist", function() {
+  var getScript = function(url) { equal(url, egc.get('gapiSourceUrl')); };
+  EGC.create({getScript: getScript});
+});
+
