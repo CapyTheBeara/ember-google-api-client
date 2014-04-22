605958cd4cfb34b397fa2cfde56eeb3f1d8eb8a3
monocle <hchw29@gmail.com>
2014-04-21 20:43:32 -0700

    utils: handleGapiResponse handles undefined response (ie, deleting file)

--- app/lib/egc/utils.js
+++ app/lib/egc/utils.js
@@ -1,7 +1,7 @@
 export function handleGapiResponse(resolve, reject, DEBUG) {
   return function(res) {
     if (DEBUG) { console.log('gapi response', res); }
-    if (res.error) { return Em.run(null, reject, res); }
+    if (res && res.error) { return Em.run(null, reject, res); }
     Em.run(null, resolve, res);
   };
 }
